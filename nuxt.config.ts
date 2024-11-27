624ad150ab25db310ade4a0f00b80b6d2c6066c1
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-11-27 18:22:39 +0900

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  srcDir: "src/",
+
   typescript: {
     typeCheck: true,
   },
+
+  modules: ["@nuxt/eslint"],
 });
