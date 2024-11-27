dd3261ef280586da2ab375bef8761f7eb5f93a50
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-11-27 18:38:17 +0900

    constant

---
+++ src/constants/data.ts
@@ -0,0 +1,16 @@
+import type { TodoType } from "../types/todo";
+
+export const INIT_TODO_LIST: Array<TodoType> = [
+  {
+    id: 1,
+    title: "Todo1",
+    content: "Todo1の内容",
+  },
+  {
+    id: 2,
+    title: "Todo2",
+    content: "Todo2の内容",
+  },
+];
+
+export const INIT_UNIQUE_ID = INIT_TODO_LIST.length;
